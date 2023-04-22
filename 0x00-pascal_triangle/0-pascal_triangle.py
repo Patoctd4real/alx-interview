bba40cb6db2c11949e772875e47d7a9515a91375
patoctd4real <patoctd4real@yahoo.com>
2023-04-22 05:46:02 +0000

    pascal

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -9,11 +9,11 @@ def pascal_triangle(n):
     if n <= 0:
         return []
 
-    result = []
+    answer = []
     for i in range(n):
         row = [1] * (i + 1)
         for j in range(1, i):
-            row[j] = result[-1][j-1] + result[-1][j]
-        result.append(row)
+            row[j] = answer[-1][j-1] + answer[-1][j]
+        answer.append(row)
 
-    return result
+    return answer
