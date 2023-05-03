f8097eb1cde798b844117855899426af9b6b6b21
patoctd4real <patoctd4real@yahoo.com>
2023-05-03 13:10:40 +0000

    checked

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-
 """
-   write a method that calculates the fewest.
+Minimum operations
 """
 
 
 def minOperations(n):
-    param_1 = 0
-    param_2 = 2
-    while n > 1:
-        while n % param_2 == 0:
-            param_1 += param_2
-            n /= param_2
-        param_2 += 1
-    return param_2
+    """
+
+    :@param n:
+    :return:
+    """
+    if n <= 1:
+        return 0
+    for x in range(2, n+1):
+        if n % x == 0:
+            return minOperations(int(n/x)) + x
