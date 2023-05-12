caee51402d8bd5bfd02790296c4405ab52dd3a5f
patoctd4real <patoctd4real@yahoo.com>
2023-05-12 00:58:29 +0000

    loging file

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,48 +1,42 @@
 #!/usr/bin/python3
-"""
-script that reads stdin line by line and computes metrics
-
-"""
+"""Python script that reads stdin line by line and computes metrics"""
 
 import sys
 
 
-def printing_metric(e_file_size, status):
-    """
-    getting the total file size and status list
-    
-    """
-    print("File size: {:d}".format(e_file_size))
-    for key, value in sorted(status.items()):
+def print_num(size_of_file, status):
+    """outputing total file size and status_t list"""
+    print("File size: {:d}".format(size_of_file))
+    for key, value in sorted(status_t.items()):
         if value != 0:
             print("{}: {}".format(key, value))
 
 
-status = {'200': 0, '301': 0, '400': 0, '401': 0,
+status_t = {'200': 0, '301': 0, '400': 0, '401': 0,
           '403': 0, '404': 0, '405': 0, '500': 0}
 
-e_file_size = 0
+size_of_file = 0
 count = 0
 try:
     for line in sys.stdin:
         args = line.split()
 
         if len(args) > 2:
-            codes_status = args[-2]
+            status_code = args[-2]
             file_size = int(args[-1])
 
-            if codes_status in status:
-                status[codes_status] += 1
+            if status_code in status_t:
+                status_t[status_code] += 1
 
-            e_file_size += file_size
+            size_of_file += file_size
             count += 1
 
             if count == 10:
-                printing_metric(e_file_size, status)
+                print_num(size_of_file, status_t)
                 count = 0
 
 except KeyboardInterrupt:
     pass
 
 finally:
-    printing_metric(e_file_size, status)
+    print_num(size_of_file, status_t)
