b0fd85b976ac4777485ce8045eb82656b91adfef
patoctd4real <patoctd4real@yahoo.com>
2023-05-12 00:49:06 +0000

    create log files

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,18 +1,24 @@
 #!/usr/bin/python3
-"""script that reads stdin line by line and computes metrics"""
+"""
+script that reads stdin line by line and computes metrics
+
+"""
 
 import sys
 
 
-def printing_metric(e_file_size, log_status):
-    """getting the total file size and status list"""
+def printing_metric(e_file_size, status):
+    """
+    getting the total file size and status list
+    
+    """
     print("File size: {:d}".format(e_file_size))
-    for key, value in sorted(log_status.items()):
+    for key, value in sorted(status.items()):
         if value != 0:
             print("{}: {}".format(key, value))
 
 
-log_status = {'200': 0, '301': 0, '400': 0, '401': 0,
+status = {'200': 0, '301': 0, '400': 0, '401': 0,
           '403': 0, '404': 0, '405': 0, '500': 0}
 
 e_file_size = 0
@@ -25,18 +31,18 @@ try:
             codes_status = args[-2]
             file_size = int(args[-1])
 
-            if codes_status in log_status:
-                log_status[codes_status] += 1
+            if codes_status in status:
+                status[codes_status] += 1
 
             e_file_size += file_size
             count += 1
 
             if count == 10:
-                printing_metric(e_file_size, log_status)
+                printing_metric(e_file_size, status)
                 count = 0
 
 except KeyboardInterrupt:
     pass
 
 finally:
-    printing_metric(e_file_size, log_status)
+    printing_metric(e_file_size, status)
