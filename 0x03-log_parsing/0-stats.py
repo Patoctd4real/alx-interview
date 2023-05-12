c42ed1d81f9ddf92198bdba9956a2cc99d02a7f3
patoctd4real <patoctd4real@yahoo.com>
2023-05-12 01:01:44 +0000

    log

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -4,18 +4,18 @@
 import sys
 
 
-def print_num(size_of_file, status):
-    """outputing total file size and status_t list"""
-    print("File size: {:d}".format(size_of_file))
-    for key, value in sorted(status_t.items()):
+def print_n(t_file_size, status):
+    """Prints total file size and status list"""
+    print("File size: {:d}".format(t_file_size))
+    for key, value in sorted(status.items()):
         if value != 0:
             print("{}: {}".format(key, value))
 
 
-status_t = {'200': 0, '301': 0, '400': 0, '401': 0,
+status = {'200': 0, '301': 0, '400': 0, '401': 0,
           '403': 0, '404': 0, '405': 0, '500': 0}
 
-size_of_file = 0
+t_file_size = 0
 count = 0
 try:
     for line in sys.stdin:
@@ -25,18 +25,18 @@ try:
             status_code = args[-2]
             file_size = int(args[-1])
 
-            if status_code in status_t:
-                status_t[status_code] += 1
+            if status_code in status:
+                status[status_code] += 1
 
-            size_of_file += file_size
+            t_file_size += file_size
             count += 1
 
             if count == 10:
-                print_num(size_of_file, status_t)
+                print_n(t_file_size, status)
                 count = 0
 
 except KeyboardInterrupt:
     pass
 
 finally:
-    print_num(size_of_file, status_t)
+    print_n(t_file_size, status)
