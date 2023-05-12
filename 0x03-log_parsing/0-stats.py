32d6a89ec8de0f6ffae3d600c61b9cbfc6fda8d4
patoctd4real <patoctd4real@yahoo.com>
2023-05-12 00:33:31 +0000

    log

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,41 +1,42 @@
 #!/usr/bin/python3
-"""script that reads stdin line by line and computes metrics"""
+"""Python script that reads stdin line by line and computes metrics"""
+
 import sys
 
 
-def output_metrics(time_file_size, log_status):
-    """output total file size and status list"""
-    print("File size: {:d}".format(time_file_size))
-    for key, value in sorted(log_status.items()):
+def print_n(t_file_size, status):
+    """Prints total file size and status list"""
+    print("File size: {:d}".format(t_file_size))
+    for key, value in sorted(status.items()):
         if value != 0:
             print("{}: {}".format(key, value))
 
 
-log_status = {'200': 0, '301': 0, '400': 0, '401': 0,
+status = {'200': 0, '301': 0, '400': 0, '401': 0,
           '403': 0, '404': 0, '405': 0, '500': 0}
 
-time_file_size = 0
+t_file_size = 0
 count = 0
 try:
     for line in sys.stdin:
         args = line.split()
 
         if len(args) > 2:
-            codes_status = args[-2]
+            status_code = args[-2]
             file_size = int(args[-1])
 
-            if codes_status in log_status:
-                log_status[codes_status] += 1
+            if status_code in status:
+                status[status_code] += 1
 
-            time_file_size += file_size
+            t_file_size += file_size
             count += 1
 
             if count == 10:
-                output_metrics(time_file_size, log_status)
+                print_n(t_file_size, status)
                 count = 0
 
 except KeyboardInterrupt:
     pass
 
 finally:
-    output_metrics(time_file_size, log_status)
+    print_n(t_file_size, status)
